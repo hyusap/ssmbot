c82b5fc9169410d3f462da63b7a5a3889e1c5a77
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-02 00:50:22 -0400

    Move bot activity into constant

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,12 @@
+const constants = {
+    STATUS_CONTENT: "over modmail"
+}
+
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        client.user.setActivity({ name: process.env.STATUS, type: 'PLAYING' });
+        client.user.setActivity({ name: constants.STATUS_CONTENT, type: 'WATCHING' });
 
         console.log("I am groot.");
     },
