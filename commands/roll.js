631c283a12141e41ab057d88e224817482a80e55
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-02 12:18:53 -0400

    Test echo and roll commands

--- commands/roll.js
+++ commands/roll.js
@@ -4,13 +4,13 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('roll')
         .setDescription('Generates a random number from 1 to the specified number (defaults to 6).')
-        .addUserOption(option => option.setName('number')
+        .addIntegerOption(option => option.setName('number')
             .setDescription('The number to roll from')
             .setRequired(false)),
     async execute(interaction) {
         // default to 6
-        const option = interaction.getUserOption('number') + 1 || 7
-        const number = Math.floor(Math.random() * (option));
+        const option = interaction.options.getInteger('number') || 6
+        const number = Math.floor(Math.random() * (option)) + 1;
 
         await interaction.reply({
             embeds: [{
